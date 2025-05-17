d92366c244210ee72ea13baed4af55fa03226cb7
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-05-17 17:49:39 +0300

    add css header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import style from "./Header.module.css";
 
 const Header = () => (
-  <header>
-    <nav>
+  <header className={style.header}>
+    <nav className={style.nav}>
+      <img src="/images/logo.svg" alt="Logo" />
+      <div className={style.navBox}>
       <Link to="/">Home</Link>
       <Link to="/catalog">Catalog</Link>
       <Link to="/favorites">Favorites</Link>
       <Link to="/bookings">My Bookings </Link>
+      </div>
     </nav>
   </header>
 );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,17 +4,20 @@ import style from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <main>
-      <section className={style.header}>
-        <span className={style.logo}>
-          Rental<span className={style.logoColor}>Car</span>
-        </span>
-        <h1>Find your perfect rental car</h1>
-        <p>Reliable and budget-friendly rentals for any journey</p>
-        
-        <Link to="/catalog">View Catalog</Link>
+      <section className={style.hero}>
+          <div className={style.contentBlock}>
+            <h1 className={style.title}>Find your perfect rental car</h1>
+            <p className={style.subtitle}>
+              Reliable and budget-friendly rentals for any journey
+            </p>
+            <Link to="/catalog" className={style.cta}>
+              View Catalog
+            </Link>
+          </div>
       </section>
     </main>
   );
 };
+
 
 export default HomePage;
